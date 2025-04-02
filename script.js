7cd96ccc1ab87c4fe0b55a57b2322a32411ec05c
Rizky Rahman <rizk87devops@gmail.com>
2025-04-03 00:06:01 +0800

    Done

---
+++ script.js
@@ -0,0 +1,33 @@
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+    let hour = new Date().getHours();
+    let minute = new Date().getMinutes();
+    let second = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (hour > 12) {
+        hour = hour - 12;
+        ampm = "PM";
+    }
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+
+    hoursEl.innerText = hour;
+    minutesEl.innerText = minute;
+    secondsEl.innerText = second;
+    ampmEl, (innerText = ampm);
+    setTimeout(() => {
+        updateClock()
+    }, 1000)
+
+
+}
+
+
+updateClock();
